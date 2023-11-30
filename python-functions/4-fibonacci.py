ee806d0fa2071a7615ad8bd1770ec65a8c823bb5
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-11-30 11:25:25 +0300

    1

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,8 @@
 def fibonacci_sequence(n):
     num1 = 0
     num2 = 1
-    print(num1)
-    print(num2)
     for i in range(2,n):
      num3 = num1 + num2 
      num1 = num2
      num2 = num3
-     print(num3)
+     print(num3, end=" ")
