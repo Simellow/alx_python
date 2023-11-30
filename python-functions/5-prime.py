0304845d6944ff92d0a80da5b6ace95ccd28ab36
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-11-30 15:58:34 +0300

    1

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,12 +1,11 @@
 def is_prime(number):
-    # number has to be greater than 1
-    number > 1
-    # divide numbers starting from 2 to number inputted 
-    for x in range (2, number):
-        if number % x == 0:
-            False
-    #stop loop if condition is met
-            break
-        else: 
-            True
+    # All numbers < 2 are not prime
+    if number < 2:
+        return False
 
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+
+    return True
+    
