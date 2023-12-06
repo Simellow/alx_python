7e314c56b687fc91a0358618523146fe8bf67b6f
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-07 00:14:29 +0300

    1

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,13 +1,5 @@
 def no_c(my_string):
-    for c in my_string:
-        if c in "Cc":
-            no_c = my_string.remove(c)
-    return no_c
-        
+    letter_c = my_string.translate({ord(i): None for i in "cC"})
+    return letter_c
 
-#         def anti_vowel(text):
-#     for c in text:
-#         if c in "aeiouAEIOU":
-#             no_vowel = text.replace(c, '')
-#     return no_vowel
-# print(anti_vowel('Hello World')
+
