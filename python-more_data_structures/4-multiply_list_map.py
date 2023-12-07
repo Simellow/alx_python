7acc359f72accdc3bf7730e3d5eef6e5d7bfd899
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-07 15:52:57 +0300

    1

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,3 +1,2 @@
-def mutiply_list_map(my_list=[], number=0):
-    my_list = [1, 2, 3, 4, 6]
+def multiply_list_map(my_list=[], number=0):
     return list(map(lambda my_list: my_list * number, my_list))
