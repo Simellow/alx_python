0d3568acf7843e022c61396b66fddf5002209042
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 14:42:02 +0300

    1

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,5 +1,5 @@
 """function that returns true if the obj is exactly an instance of the specified class and false otherwise"""
 def is_same_class(obj, a_class):
     """ using a the type method we check"""
-    return type(obj) == a_class
+    return type(obj) is a_class
 
