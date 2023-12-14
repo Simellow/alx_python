7cf46a6ea721e42d27d8996162e9b46f07c21841
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 20:13:10 +0300

    1

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,6 @@
+""" function that returns true if the obj is exactly an instance of the specified class and false otherwise"""
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if object is an instance of the
+    using the is instance method"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
