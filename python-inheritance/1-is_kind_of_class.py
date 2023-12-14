0eaebd7c0371c18d46c3a8728d38f9d9e37e1192
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 15:22:01 +0300

    1

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,5 +1,8 @@
-"""function that returns true if the obj is exactly an instance of the specified class and false otherwise"""
-def is_same_class(obj, a_class):
-   """ returns True if object is an instance of the
+#!/usr/bin/python3
+""" module 3-is_kind_of_class contains function is_kind_of_class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ returns True if object is an instance of the
     class or of a class that class inherited from """
-   return isinstance(obj, a_class)
+    return isinstance(obj, a_class)
