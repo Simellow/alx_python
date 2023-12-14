cf94c1d762291f967d6d4a4b31557c290bfa8cfb
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 18:20:43 +0300

    1

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -22,8 +22,23 @@ class Rectangle(BaseGeometry):
        
         """Validating rectangle from BaseGeometry"""
 
-        self.integer_validator('', width)
-        self.integer_validator('', height)
+        self.integer_validator("", width)
+        self.integer_validator("", height)
         self.__width = width
         self.__height = height
-        
+
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
