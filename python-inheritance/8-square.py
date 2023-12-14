6f197637e070e55d825b5500ab3d723af9a5ff45
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 20:03:07 +0300

    1

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,46 @@
+"""class called BaseGeometry"""
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """Raising exception"""
+        raise Exception ("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """to check for positive integer """
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+
+class Rectangle(BaseGeometry):
+    """class  Rectangle that inherits from BaseGeometry"""
+    
+    def __init__(self, width, height):
+       
+        """Validating rectangle from BaseGeometry"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        """area function"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """strrrrrrrr"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
+class Square(Rectangle):
+    """ defines class Square """
+
+    def __init__(self, size):
+        """ initializes empty Square and
+        validates size as a positive integer """
+        if self.integer_validator("size", size):
+            self.__size = size
+        super().__init__(size, size)
