b89b71ea9edaa513f8543ccdd78247df0f32eb2d
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 20:21:13 +0300

    1

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,7 +1,7 @@
-""" module 4-inherits_from contains function inherits_from """
+""" inherits-from """
 
 
 def inherits_from(obj, a_class):
-    """ returns True if the object in an instance of a class
-    that inherited (directly or indirectly) from a specified class """
+    """ a function that returns True if the object is an instance of 
+    a class that inherited (directly or indirectly) from the specified class and False otherwise """
     return issubclass(type(obj), a_class) and type(obj) != a_class
