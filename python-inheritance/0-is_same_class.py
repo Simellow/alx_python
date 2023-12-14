210569cd333f11f69bc0248d20c97e9b3f1c2e7b
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 14:28:55 +0300

    1

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,7 +1,7 @@
 """function that returns true if the obj is exactly an instance of the specified class and false otherwise"""
 def is_same_class(obj, a_class):
-    """checking if object is in a_class using an if else statement"""
-    isinstance(obj, a_class)
+    """checking if object is in a_class using ..."""
+    return isinstance(obj, a_class)
 
 # a = 1
 # if is_same_class(a, int):
@@ -10,3 +10,4 @@ def is_same_class(obj, a_class):
 #     print("{} is an instance of the class {}".format(a, float.__name__))
 # if is_same_class(a, object):
 #     print("{} is an instance of the class {}".format(a, object.__name__))
+
