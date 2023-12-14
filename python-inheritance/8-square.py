956d0e0d246d83bbb1c1c8b976248ba6f499e00f
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-14 20:09:40 +0300

    1

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,39 +1,7 @@
-"""class called BaseGeometry"""
+""" module 10-square contains subclass Square
+which inherits from class Rectangle """
 
-class BaseGeometry:
-    """BaseGeometry"""
-    def area(self):
-        """Raising exception"""
-        raise Exception ("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """to check for positive integer """
-        if type(value) != int:
-            raise TypeError('{} must be an integer'.format(name))
-        elif value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
-
-
-class Rectangle(BaseGeometry):
-    """class  Rectangle that inherits from BaseGeometry"""
-    
-    def __init__(self, width, height):
-       
-        """Validating rectangle from BaseGeometry"""
-
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-    
-    def area(self):
-        """area function"""
-        return self.__width * self.__height
-    
-    def __str__(self):
-        """strrrrrrrr"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('7-rectangle').Rectangle
     
 class Square(Rectangle):
     """ defines class Square """
