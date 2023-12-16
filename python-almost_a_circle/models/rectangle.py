5e83a5deda5762a97a17b0e7a090ee856d589728
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-16 16:33:22 +0300

    1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -24,7 +24,11 @@ class Rectangle(Base):
     
     @width.setter
     def width(self, value):
-        """setter for width"""
+        """setter for width where errors and/or exceptions are handled"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
     # width got and set
 
@@ -35,7 +39,11 @@ class Rectangle(Base):
     
     @height.setter
     def height(self, value):
-        """setter for height"""
+        """setter for height where errors and/or exceptions are handled"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
     # height got and set
 
@@ -46,7 +54,11 @@ class Rectangle(Base):
     
     @x.setter
     def x(self, value):
-        """setter for x"""
+        """setter for x where errors and/or exceptions are handled"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
     # x got and set
 
@@ -57,7 +69,11 @@ class Rectangle(Base):
     
     @y.setter
     def y(self, value):
-        """setter for height"""
+        """setter for y where errors and/or exceptions are handled"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
     # y got and set
     
