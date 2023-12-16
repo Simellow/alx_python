7e2e83675407b65bf9d276c9b88db6c99485a442
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-16 15:22:18 +0300

    1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,10 +1,16 @@
 """Defining our class Base"""
 class Base:
+
     """private instance attribute initialization"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """constructor for base"""
         self.id = id
+        """if id is not "None", we assign self.id to id
+        otherwise we update our private class attribute.
+         We finally assign the new value to the public 
+         instance attribute id"""
         if id is not None:
             self.id = id
         else: 
