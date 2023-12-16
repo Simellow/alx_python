c8aeee3a9d2078fc82a4e8284cd2e7ef43b2a311
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-16 17:16:08 +0300

    1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,4 @@
-"""importing the class Base"""
+"""importing the class 'Base' """
 from models.base import Base
 
 class Rectangle(Base):
@@ -19,7 +19,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """setter for width where height input must be positive integer"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value <= 0:
             raise ValueError("width must be > 0")
@@ -33,7 +33,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """setter for height where height input must be positive integer"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value <= 0:
             raise ValueError("height must be > 0")
@@ -47,7 +47,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """setter for x where x input must be an integer and greater than 0"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         elif value < 0:
             raise ValueError("x must be >= 0")
@@ -61,8 +61,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """setter for y where y input must be an integer and greater than 0"""
-        if type(value) is not int:
-            raise TypeError('y must be an integer')
+        if type(value) != int:
+            raise TypeError("y must be an integer")
         elif value < 0:
-            raise ValueError('y must be >= 0')
+            raise ValueError("y must be >= 0")
         self.__y = value
