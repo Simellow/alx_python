e0a86ed1231a25507b9545a5ed8cb7b3b2c5b7c3
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-19 11:41:31 +0300

    1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,20 +7,20 @@ class Square(Rectangle):
         """inheriting our constructor using our super class
         width and height will be assigned to the value of size"""
         super().__init__(size, size, x, y, id)
-        self.size = size
     
     @property
     def size(self):
         """getter for size"""
-        return self.width and self.height
+        return self.width
 
     @size.setter
     def size(self, value):
-        """setting our height and width"""
+        """setting our height and width to equal values"""
         self.height = value
         self.width = value
     
     def __str__(self):
         """informal string representation of the square"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.width or self.height)
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x, self.y, self.width)
+
     
