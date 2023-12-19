160f15428e2e6e16c4f0a8f508d4db2d25e0abf4
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-19 20:21:18 +0300

    1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,9 +9,12 @@
 
 # url = "https://alu-intranet.hbtn.io/status"
 # response = requests.get(url)
-
-"""python"""
-import requests
-""" module documented"""
-response = requests.get("https://alu-intranet.hbtn.io/status")
-
+"""nknk"""
+import urllib.request as req
+if __name__ == "__main__":
+    with req.urlopen('https://alu-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
