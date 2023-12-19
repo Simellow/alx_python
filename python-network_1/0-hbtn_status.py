7024bde4ae52245b1192310e6135c2b6cbe3126f
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-19 20:17:11 +0300

    1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,15 +5,13 @@
 # """ """
 # requests.get(url)
 #!/usr/bin/python3
+# import requests
+
+# url = "https://alu-intranet.hbtn.io/status"
+# response = requests.get(url)
+
+# python
 import requests
 
-url = "https://alu-intranet.hbtn.io/status"
-response = requests.get(url)
-
-if response.status_code == 200:
-    print("Body response:")
-    for line in response.text.splitlines():
-        print("\t- " + line)
-else:
-    print("Error: Failed to fetch the URL. Status code:", response.status_code)
+response = requests.get("https://alu-intranet.hbtn.io/status")
 
