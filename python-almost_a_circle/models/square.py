473c7f3648fec487240a464e65cd87f420b75c04
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-19 11:33:53 +0300

    1

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """getter for size"""
-        return self.width
+        return self.width and self.height
 
     @size.setter
     def size(self, value):
@@ -22,4 +22,29 @@ class Square(Rectangle):
     
     def __str__(self):
         """informal string representation of the square"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width or self.height)
+    
+    def update(self, *args, **kwargs):
+        """update"""
+        i = 0
+        if args:
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+                i += 1
+        else:
+            for arg in kwargs:
+                setattr(self, arg, kwargs.get(arg))
+
+    def to_dictionary(self):
+        """
+            returns the dictionary
+            representation of a square
+        """
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
