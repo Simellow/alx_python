ec75269e2336b51959cf0043230e8e398e3c437e
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-20 17:00:31 +0300

    1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,6 +5,9 @@ Usage: ./1-hbtn_header.py <URL>
 import requests
 import sys
 
-if __name__ == '__main__':
-    with requests.urlopen(sys.argv[1]) as response:
-        print("{}".format(response.getheader('X-Request-Id')))
+url = sys.argv[1]
+response = requests.get(url)
+
+x_request_id = response.headers.get('X-Request-Id')
+print("X-Request-Id:", x_request_id)
+
