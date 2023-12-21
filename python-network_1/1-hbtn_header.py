0099136b50fc6d248c185ceebd3843b32f26f94e
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-21 14:33:15 +0300

    1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,13 +1,39 @@
-"""Displays the X-Request-Id header variable of a request to a given URL.
+# """CHECK"""
+# import requests
+# import sys
 
-Usage: ./1-hbtn_header.py <URL>
-"""
+# url = sys.argv[1]
+# response = requests.get(url)
+
+# x_request_id = response.headers.get('X-Request-Id')
+# print("X-Request-Id:", x_request_id)
+
+
+# """ takes a URL, sends a request and returns"""
+
+
+# import urllib.request
+# import sys
+
+# if __name__ == '__main__':
+#     with urllib.request.urlopen(sys.argv[1]) as response:
+#         print("{}".format(response.getheader('X-Request-Id')))
+
+"""check"""
 import requests
 import sys
 
-url = sys.argv[1]
-response = requests.get(url)
+def get_request_id(url):
+    response = requests.get(url)
+    request_id = response.headers.get('X-Request-Id')
+    return request_id
 
-x_request_id = response.headers.get('X-Request-Id')
-print("X-Request-Id:", x_request_id)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide a URL as an argument.")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    request_id = get_request_id(url)
+    print("X-Request-Id:", request_id)
 
--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -4,7 +4,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+
+auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+
+r = requests.get("https://api.github.com/user", auth=auth)
+
+print(r.json().get("id"))
