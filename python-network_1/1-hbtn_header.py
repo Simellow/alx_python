e37652596529eb0cb8822c9e43a38cca5b19b88d
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2023-12-21 14:38:19 +0300

    1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,35 +5,7 @@ import sys
 url = sys.argv[1]
 response = requests.get(url)
 
-x_request_id = response.headers.get('X-Request-Id')
-print(x_request_id)
+request = response.headers.get('X-Request-Id')
+print(request)
 
-
-# """ takes a URL, sends a request and returns"""
-
-
-# import urllib.request
-# import sys
-
-# if __name__ == '__main__':
-#     with urllib.request.urlopen(sys.argv[1]) as response:
-#         print("{}".format(response.getheader('X-Request-Id')))
-
-# """check"""
-# import requests
-# import sys
-
-# def get_request_id(url):
-#     response = requests.get(url)
-#     request_id = response.headers.get('X-Request-Id')
-#     return request_id
-
-# if __name__ == "__main__":
-#     if len(sys.argv) < 2:
-#         print("Please provide a URL as an argument.")
-#         sys.exit(1)
-
-#     url = sys.argv[1]
-#     request_id = get_request_id(url)
-#     print(request_id)
 
