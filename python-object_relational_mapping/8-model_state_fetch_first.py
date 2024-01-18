0d28b8b001360470a03950597657bd0366ee144d
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-18 22:01:53 +0300

    1

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+'''importing sqlalhemy, sys, and model_state'''
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+path = "mysql+mysqldb://{}:{}@localhost/{}"
+engine = create_engine(path.format(argv[1], argv[2], argv[3]))
+
+Session = sessionmaker(bind = engine)
+session = Session()
+
+states = session.query(State).order_by(State.id).first()
+if states:
+    print("{}: {}".format(states.id, states.name))
+else:
+    print("Nothing")
