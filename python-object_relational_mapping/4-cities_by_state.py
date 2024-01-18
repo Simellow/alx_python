2afce837767c2969efa2d72d01db498e51d2da38
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-18 14:28:21 +0300

    1

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 cur = db.cursor()
 
 # executing a script that lists all cities from the database
-cur.execute("SELECT cities.id, cities.name, states.name FROM citiesINNER JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+cur.execute("SELECT cities.id, cities.name, states.name FROM cities, states WHERE cities.state_id = states.id ORDER BY cities.id ASC")
 
 #printing result 
 myresult = cur.fetchall()
