409b3b68e9bafeea3e2643fd5ead60072ebbc47d
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-18 11:14:06 +0300

    1

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,6 @@
 #import MySQLdb & sys
 import MySQLdb
-from sys import argv
+import sys
 
 #connecting to mysqldb
 db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
@@ -8,7 +8,7 @@ db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 #getting a cursor 
 cur = db.cursor()
 
-cur.execute("SELECT * FROM states WHERE name= %s ORDER BY states.id ASC".format(argv[4], ))
+cur.execute("SELECT * FROM states WHERE name= '{}' ORDER BY states.id ASC".format(sys.argv[4]))
 
 
 
