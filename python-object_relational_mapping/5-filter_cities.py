8c7bee2f205f9e401f2c957540ff577982af3d2e
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-18 22:37:56 +0300

    1

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,42 @@
-#import MySQLdb & sys
+# A script that takes in the name of a state as an argument and lists all cities of that state, 
+import sys
 import MySQLdb
-from sys import argv
 
-#connecting to mysqldb
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+# Get MySQL username, password, database name, and state name from command-line arguments
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+state_name = sys.argv[4]
 
-#getting a cursor 
-cur = db.cursor()
+# Connect to the MySQL server
+db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
 
-# executing a script that lists all cities from the database
-cur.execute("SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id",(argv[4], ))
+# Create a cursor
+cursor = db.cursor()
 
-#printing result 
-myresult = cur.fetchall()
-for state in myresult:
-  print(state)
+# Execute the query to retrieve all cities of the given state
+query = f"""
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id ASC
+"""
+cursor.execute(query, (state_name,))
+
+# Fetch all the results
+results = cursor.fetchall()
+
+# Check if any results were found
+if len(results) == 0:
+    print(f"No cities found for the specified state '{state_name}'.")
+else:
+    # Print the cities
+    cities = [row[0] for row in results]
+    print(', '.join(cities))
 
 
-# Closing all cursors and databases 
-cur.close()
-db.close() 
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
