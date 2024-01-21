ba735cf0f994f0fa297d8cbb929b0ddc0ea60192
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-21 21:17:57 +0300

    1

---
+++ python-web_framework/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Importing Flask form flask'''
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = escape(text).replace("_", " ")
+    return f'C {text}'
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    text = escape(text).replace("_", " ")
+    return 
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
+# /python/<text>: display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+# The default value of text is “is cool”
+
