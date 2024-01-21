9b335c0c4e3040f90ed97fd01a8fa4d79229e03c
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-21 20:28:19 +0300

    1

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Importing Flask form flask'''
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_user_profile(text):
+    return f'C {escape(text)}'
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
