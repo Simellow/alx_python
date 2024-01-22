1e6cb42d29bcfd4989eb1576fbe872b562b3a04c
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-22 22:39:22 +0300

    1

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -15,7 +15,8 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    return f'C {text}'(text.replace("_", " "))
+    text = escape(text).replace("_", " ")
+    return f'C {text}'
 
 
 if __name__ == "__main__":
