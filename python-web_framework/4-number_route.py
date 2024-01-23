a7bcd8bb7b102d0ec3a2b5ec4af9f489f994c999
Simelo Aberra <149304514+Simellow@users.noreply.github.com>
2024-01-23 09:51:09 +0300

    1

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -23,6 +23,7 @@ def python(text ='is cool'):
     return 'Python {}'.format((text).replace("_", " "))
 
 @app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/', strict_slashes=False)
 def number(n):
     if n == int:
         return '{} is a number'.format(n)
